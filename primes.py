c9cad8e3a48f9bdb0eda1645436812442a5421de
Jude Lawrence <jude.lawrence@kcl.ac.uk>
2022-12-09 14:49:20 +0000

    Solved

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if (number_of_primes < 1):
+        raise ValueError("Number of primes must be greater than 0.")
     list = []
+    number =2
+
+    while len(list) < number_of_primes:
+        is_prime = True
+
+        for prime in list:
+            if number % prime ==0:
+                is_prime = False
+                break
+
+        if is_prime:
+            list.append(number)
+
+        number += 1
+
+
+
     return list
